74138540cec66c5e8d77023f47b15ecd81c953bc
Joshua Draper <josh.drapergame@gmail.com>
2019-09-16 20:32:51 -0600

    plans are to make 2 sites, 1 withandwithout react

---
+++ react-website/src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Title = () => {
+
+    return(
+        <div>
+            <h3>
+                Joshua Draper
+            </h3>
+            {/* https://learn.lambdaschool.com/web2/module/rec2TJ1vrdfcnx2EG/ for react-router */}
+        </div>
+    )
+}
+
+export default Title;
